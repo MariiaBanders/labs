a095b852c65b79b0ba7e7e45d02606cdd09adf63
hryhorchuk117 <hryhorchuk117@gmail.com>
2020-06-04 16:12:36 +0300

    lab 4 added

--- lab3/main.java
+++ lab3/main.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class main {
 
 	public static void main(String[] args) throws Exception {
-		Lexema lex = new Lexema("code.txt");
+		Lexema lex = new Lexema("/home/hryhorchuk117/Documents/labs/lab3/code.txt");
 		FileWriter fw = new FileWriter("result.txt");
 		//fw.write("Counter Type Lexema\n");
 		System.out.println("Counter Type Lexema");
